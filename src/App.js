241bfb67bf6a78134381c477efce6695b7da3ded
Fakhruddin <fakhruddin.piaic@outlook.com>
2022-12-11 00:39:37 +0500

    Class 3 Part1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import './App.css';
+import Dinner from './dinner.js';
 
 
 function App() {
   return (
-    
+    <div className="App">
+      <header className="App-header">
+    <Dinner dishname="Haleem" sweet="Kheer"/>
+    <Dinner dishname="karhai" sweet="Jalibi"/>
+    <Dinner dishname="Daal" sweet="Gajar ka Halwa"/>
       <h1>Hello World!</h1>
-    
+      </header>
+    </div>
   );
 }
 
