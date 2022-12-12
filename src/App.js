6e5aba2914a4ed5b2fb6394378bd79a3a6d37aa7
Fakhruddin Kaukawala <fakhruddin@jafferjees.com>
2022-12-12 11:20:54 +0500

    Change in Css

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Counter from './Counter.js';
 
 
-
-
 function App() {
-  
+
   let [count, setCount] = useState(1);
   return (
     <div className="App">
       <header className="App-header">
-  
-      <h1>Value of counter variable is : {count} </h1>
-      <Counter countervalue={count}/>
-      <button onClick={
-        ()=> setCount(count+1)
-      }>
-        Update counter
-      </button>
+
+        <h1>Value of counter variable is : {count} </h1>
+        <Counter countervalue={count} />
+        <button className="App-Button" onClick={
+          () => setCount(count + 1)
+        }>
+          Update counter
+        </button>
       </header>
-      
+
     </div>
   );
 }
--- src/Counter.js
+++ src/Counter.js
@@ -1,10 +1,9 @@
 import React from 'react';
 
-export default function Counter(props)
-{
-return(
-    <h2>Value of counter get from component : {props.countervalue} </h2>
-)
+export default function Counter(props) {
+    return (
+        <h3>Value of counter get from component : {props.countervalue} </h3>
+    )
 }
 
 
