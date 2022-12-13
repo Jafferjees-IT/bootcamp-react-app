f83dcdcb8d81140feb43331ba6a211c8864b10c7
Fakhruddin Kaukawala <fakhruddin@jafferjees.com>
2022-12-13 17:53:58 +0500

    update background

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ function App() {
   let [count, setCount] = useState(1);
   let [isMorning, setMorning] = useState(true)
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className='App'>
+      <header className={`App-header ${isMorning ? 'bg' : 'App-header'}`}>
         <h1 className={`App-Button ${isMorning ? 'dayLight' : 'App-Button'}`}>Number Type = {isMorning ? 'Odd' : 'Even'}</h1>
         <h1>Value of counter variable is : {count} </h1>
         <Counter countervalue={count} />
